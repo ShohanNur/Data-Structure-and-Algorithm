dd62c5e778b9f7c592ca93ca95a012377a5c0940
ShohanNur <shohanur.rahman133883@gmail.com>
2022-04-29 01:19:41 +0600

    Updated codes

--- Algorithm CSE237/DFS/Bipartite or Not.cpp
+++ Algorithm CSE237/DFS/Bipartite or Not.cpp
@@ -14,12 +14,11 @@ bool dfs(int node,int col)
         int child=graph[node][i];
         if(vis[child]==0)
         {
-            if(dfs(child, col ^ 1)==false){
-                return false;
-            }
+            bool res = dfs(child , col ^ 1);
+            if(res == false) return false;
         }
         else
-            if(color[node]=color[child])
+            if(color[node]==color[child])
               return false;
     }
     return true;
@@ -30,9 +29,9 @@ int main()
 
 int n,e;
 cin>>n;
-while(n){
 cin>>e;
-for(int i=0;i<n;i++)
+
+for(int i=1;i<=n;i++)
 {
     graph[i].clear();
     vis[i]=0;
@@ -41,32 +40,32 @@ while(e--)
 {
     int x,y;
     cin>>x>>y;
-    x--,y--;
+  //  x--,y--;
     graph[x].push_back(y);
     graph[y].push_back(x);
 }
-
 bool ok = true;
-for(int i=0;i<n;i++){
-if(dfs(i,-1)==false)
-{
-    ok = false;
-}
+for(ll i=1;i<=n;i++){
+    if(!vis[i]){
+        if(dfs(i,0)==false){
+            ok = false;
+        }
+    }
 }
 if(ok==false)
-    cout<<"Tree is Bipartite"<<endl;
-
+    cout<<"Tree is not Bipartite"<<endl;
 else
-     cout<<"Tree is not Bipartite"<<endl;
+     cout<<"Tree is Bipartite"<<endl;
+
 
 }
-}
+
 /*
-5 6
-0 1
-2 0
-2 1
+6 6
+1 2 
 1 3
 3 4
-1 4
+3 5
+2 6
+4 5
 */
