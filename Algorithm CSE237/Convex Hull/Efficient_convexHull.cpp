10cc11e71c7caf026807fe13f120c295aba38094
ShohanNur <shohanur.rahman133883@gmail.com>
2022-05-18 23:42:00 +0600

    convex hull added

---
+++ Algorithm CSE237/Convex Hull/Efficient_convexHull.cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <climits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <cstring>
+
+
+#define pb       push_back
+#define pf       push_front
+#define pi       2*acos(0.0)
+#define f        first
+#define s        second
+#define inf      1e9
+#define endl     '\n'
+#define mod      1000000007
+#define all(x)   x.begin(), x.end()
+#define rev(x)   x.rbegin(),x.rend()
+#define spc(x)   cout<<fixed<<setprecision(x)
+#define fastio   {ios_base::sync_with_stdio(false);cin.tie(NULL);}
+#define test     ll t; cin>>t; while(t--)
+#define sz       100003
+
+typedef long long ll;
+
+using namespace std;
+using namespace std::chrono;
+
+
+int xx,yy;
+class Point    {
+public:
+    int x, y;
+
+    // comparison is done first on y coordinate and then on x coordinate
+    bool operator < (Point b) {
+        if (y != b.y)
+            return y < b.y;
+        return x < b.x;
+    }
+};
+
+// Point having the least y coordinate, used for sorting other points
+// according to polar angle about this point
+Point pivot;
+
+// returns -1 if a -> b -> c forms a counter-clockwise turn,
+// +1 for a clockwise turn, 0 if they are collinear
+int ccw(Point a, Point b, Point c) {
+    int area = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+    if (area > 0)
+        return -1;
+    else if (area < 0)
+        return 1;
+    return 0;
+}
+
+// returns square of Euclidean distance between two points
+int sqrDist(Point a, Point b)  {
+    int dx = a.x - b.x, dy = a.y - b.y;
+    return dx * dx + dy * dy;
+}
+
+// used for sorting points according to polar order w.r.t the pivot
+bool POLAR_ORDER(Point a, Point b)  {
+    int order = ccw(pivot, a, b);
+    if (order == 0)
+        return sqrDist(pivot, a) < sqrDist(pivot, b);
+    return (order == -1);
+}
+
+stack<Point> grahamScan(Point *points, int N)    {
+    stack<Point> hull;
+
+    if (N < 3)
+        return hull;
+
+    // find the point having the least y coordinate (pivot),
+    // ties are broken in favor of lower x coordinate
+    int leastY = 0;
+    for (int i = 1; i < N; i++)
+        if (points[i] < points[leastY])
+            leastY = i;
+
+    // swap the pivot with the first point
+    Point temp = points[0];
+    points[0] = points[leastY];
+    points[leastY] = temp;
+
+    // sort the remaining point according to polar order about the pivot
+    pivot = points[0];
+    sort(points + 1, points + N, POLAR_ORDER);
+
+    hull.push(points[0]);
+    hull.push(points[1]);
+    hull.push(points[2]);
+
+    for (int i = 3; i < N; i++) {
+        Point top = hull.top();
+        hull.pop();
+        while (ccw(hull.top(), top, points[i]) != -1)   {
+            top = hull.top();
+            hull.pop();
+        }
+        hull.push(top);
+        hull.push(points[i]);
+    }
+    return hull;
+}
+
+int main()  {
+
+    int N;
+    cin>>N;
+    Point points[N+1];
+    for(int i=0;i<N;i++){
+       cin>>xx>>yy;
+       points[i].x=xx;
+       points[i].y=yy;
+    }
+    //int N = sizeof(points)/sizeof(points[0]);
+
+    stack<Point> hull = grahamScan(points, N);
+    while (!hull.empty())   {
+        Point p = hull.top();
+        hull.pop();
+
+        printf("(%d, %d)\n", p.x, p.y);
+    }
+
+    return 0;
+}
