3acc644493c05b2b2c3c50c21cf6036d04a2abc5
ShohanNur <shohanur.rahman133883@gmail.com>
2022-06-06 00:08:22 +0600

    LCS bottom up added

---
+++ Algorithm CSE237/Dynamic Programming/LCS_bottomUp.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <climits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <cstring>
+
+
+#define pb       push_back
+#define pf       push_front
+#define pi       2*acos(0.0)
+#define f        first
+//#define s        second
+#define inf      1e18
+#define mod      1000000007
+#define all(x)   x.begin(), x.end()
+#define rev(x)   x.rbegin(),x.rend()
+#define spc(x)   cout<<fixed<<setprecision(x)
+#define fastio   {ios_base::sync_with_stdio(false);cin.tie(NULL);}
+#define test     ll t; cin>>t; while(t--)
+#define NIL      -1
+#define MAX      1000002
+
+typedef long long ll;
+using namespace std;
+
+string s,ss;
+
+void lcs(string S1, string S2, int m, int n) {
+  int LCS_table[m + 1][n + 1];
+
+
+  // Building the mtrix in bottom-up way
+  for (int i = 0; i <= m; i++) {
+    for (int j = 0; j <= n; j++) {
+      if (i == 0 || j == 0)
+        LCS_table[i][j] = 0;
+      else if (S1[i - 1] == S2[j - 1])
+        LCS_table[i][j] = LCS_table[i - 1][j - 1] + 1;
+      else
+        LCS_table[i][j] = max(LCS_table[i - 1][j], LCS_table[i][j - 1]);
+    }
+  }
+
+  int index = LCS_table[m][n];
+  char lcsAlgo[index + 1];
+  lcsAlgo[index] = '\0';
+
+  int i = m, j = n;
+  while (i > 0 && j > 0) {
+    if (S1[i - 1] == S2[j - 1]) {
+      lcsAlgo[index - 1] = S1[i - 1];
+      i--;
+      j--;
+      index--;
+    }
+
+    else if (LCS_table[i - 1][j] > LCS_table[i][j - 1])
+      i--;
+    else
+      j--;
+  }
+  
+  // Printing the sub sequences
+  cout << "string 1 : " << S1 << "\nstring 2 : " << S2 << "\nLCS: " << lcsAlgo << "\n";
+}
+
+int main() {
+  cin>>s>>ss;
+  int m = s.size();
+  int n = ss.size();
+
+  lcs(s, ss , m, n);
+}
