9b0fc9247a10d3d7fb23ebaf9f12c84e578131e9
ShohanNur <shohanur.rahman133883@gmail.com>
2022-07-07 00:35:48 +0600

    updated

---
+++ Algorithm CSE237/BFS/BFS_on_2d_grid.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <climits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <cstring>
+
+
+#define pb       push_back
+#define pf       push_front
+#define pi       2*acos(0.0)
+#define f        first
+#define s        second
+#define inf      1e18
+#define mod      1000000007
+#define all(x)   x.begin(), x.end()
+#define rev(x)   x.rbegin(),x.rend()
+#define spc(x)   cout<<fixed<<setprecision(x)
+#define fastio   {ios_base::sync_with_stdio(false);cin.tie(NULL);}
+#define test     ll t; cin>>t; while(t--)
+
+typedef unsigned long long ll;
+
+using namespace std;
+using namespace std::chrono;
+ll r,c;
+ll vis[1003][1003];
+int dx[]={-1,0,1,0};
+int dy[]={0,1,0,-1};
+ll dis[1001][1001];
+bool isValid(ll x, ll y){
+    if(x<1 || x>r || y<1 || y>c){
+      return false;
+    }
+    if(vis[x][y]==true){
+      return false;
+    }
+
+    return true;
+}
+void dfs(ll x, ll y){
+    queue<pair<ll,ll>>q;
+    q.push({x,y});
+    vis[x][y]=1;
+    dis[x][y]=0;
+
+    while(!q.empty()){
+        int parentx = q.front().first;
+        int parenty = q.front().second;
+
+        q.pop();
+
+        for(int i=0;i<4;i++){
+           if(isValid(parentx+dx[i],parenty+dy[i])){
+               int childx = parentx+dx[i];
+               int childy = parenty+dy[i];
+               vis[childx][childy] = 1;
+               dis[childx][childy] = dis[parentx][parenty]+1;
+               q.push({childx,childy});
+           }
+        }
+    }
+
+    for(int i=1;i<=r;i++){
+      for(int j=1;j<=c;j++){
+        cout<<dis[i][j]<<" ";
+      }
+      cout<<endl;
+    }
+}
+int main(){
+    cin>>r>>c;
+    int x,y;
+    cin>>x>>y;
+    dfs(x,y); 
+}
