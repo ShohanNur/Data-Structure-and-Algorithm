0f33aa15a6338b073687cdb70910606bad493f40
ShohanNur <shohanur.rahman133883@gmail.com>
2022-07-18 21:35:09 +0600

    updated

---
+++ Algorithm CSE237/Number Theory/EGCD.cpp
@@ -0,0 +1,74 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <climits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <cstring>
+
+
+#define pb       push_back
+#define pf       push_front
+#define pi       2*acos(0.0)
+#define f        first
+#define s        second
+#define inf      1e18
+#define mod      1000000007
+#define all(x)   x.begin(), x.end()
+#define rev(x)   x.rbegin(),x.rend()
+#define spc(x)   cout<<fixed<<setprecision(x)
+#define fastio   {ios_base::sync_with_stdio(false);cin.tie(NULL);}
+#define test     ll t; cin>>t; while(t--)
+
+typedef long long ll;
+
+using namespace std;
+using namespace std::chrono;
+
+
+int egcd(int a, int b, int &x, int &y){
+   if(b==0){
+     x = 1;
+     y = 0;
+     return a;
+   }
+
+   int x1, y1;
+   int d = egcd(b,a%b,x1,y1);
+   x = y1;
+   y = x1 - y1*(a/b); 
+   cout<<x<<" "<<y<<endl;
+   return d;
+}
+int main(){
+  int a,b,x,y;
+  cin>>a>>b;
+
+  cout<<egcd(a,b,x,y);
+}
